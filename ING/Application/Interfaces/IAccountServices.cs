fa9c639d16ae0b3d653c65c525b78ec5bd42abe9
Stefanuca Ionel <52784828+istef94@users.noreply.github.com>
2021-04-22 23:04:10 +0300

    dfgd

--- ING/Application/Interfaces/IAccountServices.cs
+++ ING/Application/Interfaces/IAccountServices.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Application.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,6 @@ namespace Application.Interfaces
 {
     interface IAccountServices
     {
+        IEnumerable<Account> GetAccountsList();
     }
 }
