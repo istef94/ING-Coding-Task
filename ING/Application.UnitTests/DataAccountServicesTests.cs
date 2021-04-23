b72fbffe38eb5bb49596ad27b0e89b83644454c2
Stefanuca Ionel <52784828+istef94@users.noreply.github.com>
2021-04-23 19:54:06 +0300

    update testing

---
+++ ING/Application.UnitTests/DataAccountServicesTests.cs
@@ -0,0 +1,38 @@
+using Application.Interfaces;
+using Infrastructure;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using System.IO;
+
+namespace Application.UnitTests
+{
+    public class DataAccountServicesTests
+    {
+        IDataAccountServices _dataAccountServices;
+
+        [SetUp]
+        public void Setup()
+        {
+            var configuration = new ConfigurationBuilder()
+              .SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile("appsettingsTestingVersion.json", optional: false, reloadOnChange: true).Build();
+
+            _dataAccountServices = new JSONDataAccountServices(configuration);
+        }
+
+        [Test]
+        public void AccountDontExist()
+        {
+            var account = _dataAccountServices.GetAccountByIBAN("Don't exist");
+            Assert.IsNull(account);
+        }
+
+        [Test]
+        public void ReceiveAccountwithIBAN()
+        {
+            var account = _dataAccountServices.GetAccountByIBAN("NL69INGB0123456789");
+            Assert.IsNotNull(account);
+            Assert.AreEqual(account.Iban, "NL69INGB0123456789");
+        }
+    }
+}
